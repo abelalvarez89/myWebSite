8546dae1b6a8ab074c8372baf3c1cc7d5547b77e
abelalvarez89 <abelalvarez89@hotmail.com>
2015-06-11 10:48:38 -0400

    Polishing

--- js/app.js
+++ js/app.js
@@ -1,7 +1,61 @@
 var app = angular.module('app', []);
 app.controller("PanelController",  function(){
 	this.tab = 1;
-	this.selectTab = function(selectTab){
+	this.selectTab = function(setTab){
 		this.tab = setTab;
+	};
+	this.isSelected = function(checkTab){
+		return this.tab === checkTab;
+	};
+});
+
+
+
+app.directive('homePage', function(){
+	return{
+		restrict: 'E',
+		templateUrl: 'public/home.html'
+	};
+});
+
+app.directive('aboutPage', function(){
+	return{
+		restrict: 'E',
+		templateUrl: 'public/about.html'
+	};
+});
+
+app.directive('projectsPage', function(){
+	return{
+		restrict: 'E',
+		templateUrl: 'public/projects.html'
+	};
+});
+
+app.directive('downloadsPage', function(){
+	return{
+		restrict: 'E',
+		templateUrl: 'public/downloads.html'
+	};
+});
+
+/*app.directive('toolsPage', function(){
+	return{
+		restrict: 'E',
+		templateUrl: 'public/tools.html'
+	};
+});
+*/
+app.directive('teachingPage', function(){
+	return{
+		restrict: 'E',
+		templateUrl: 'public/teaching.html'
+	};
+});
+
+app.directive('contactPage', function(){
+	return{
+		restrict: 'E',
+		templateUrl: 'public/contact.html'
 	};
 });
