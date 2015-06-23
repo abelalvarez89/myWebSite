833d8bd03773537c1f4e6b3bd1b67ce82da7656f
abelalvarez89 <abelalvarez89@hotmail.com>
2015-06-23 16:43:48 -0400

    Angular version working

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', []);
+var app = angular.module('pages', []);
 app.controller("PanelController",  function(){
 	this.tab = 1;
 	this.selectTab = function(setTab){
@@ -9,6 +9,180 @@ app.controller("PanelController",  function(){
 	};
 });
 
+app.controller("LocationController", function(){
+
+  this.productObj = expensesObject;
+
+  this.getDate = function(){
+    this.productObj.showDate = true;
+
+    var x = new Date();
+    this.productObj.date = x.toLocaleString();
+  };
+
+  this.isOther = function(store){
+    return store == "Other";
+  };
+
+});
+/*
+app.controller('EmailController', function(){
+  this.email = false;
+  this.isEmail = function(){
+    return this.email;
+  }
+  this.showForm = function(){
+    this.email = true;
+  };
+
+  this.cancelForm = function(){
+    this.email = false;
+  }
+
+});
+*/
+
+app.controller('ReviewController', function() {
+  this.review = {};
+  this.toShow = true;
+  this.outputObj = localObj;
+  this.sum = 0;
+  
+  this.setValues = function(indexs,others, totals, dates){
+    if(typeof(localStorage.indexing) == "undefined"){
+      localStorage.indexing = 0;
+      var t = [];
+      localStorage["testing"] = JSON.stringify(t);
+    }
+    if(totals == null){
+      totals = 0;
+    }
+    if(indexs == "Other"){
+      indexs = others;
+    }
+    localStorage.showOutput = "true";
+    localStorage.store = indexs;
+    localStorage.total = totals;
+    localStorage.date = (new Date()).toLocaleString();
+  };
+
+    this.showOutput = function(){
+      
+      var str = JSON.parse(localStorage["testing"]);
+      if(str.length == 0){
+        this.outputObj = [];
+        return false;
+      
+      }
+      var res = str.split("//");
+      var i = 0;
+      this.outputObj = [];
+      this.sum = 0;
+      for (i = 1; i < res.length; i++) { 
+        var temp = {
+          store: {},
+        total: {},
+        date: {},
+        index: 0
+      };
+          var x = res[i].split(">>");
+        temp.store = x[0];
+        var noComma = x[1].split(",");
+        temp.total = noComma[noComma.length-1];
+        this.sum  = this.sum + parseFloat(temp.total);
+        noComma = x[2].split(",");
+        temp.date =noComma[noComma.length-2] + " " + noComma[noComma.length-1];
+        this.outputObj.push(temp);
+        temp.index = i;
+    }
+    };
+
+    if(localStorage.indexing == "0"){
+      localStorage.showOutput = "true";
+      this.showOutput();
+    }
+
+    this.cleanHistory = function(){
+      var t = [];
+      localStorage["testing"] = JSON.stringify(t);
+      localStorage.showOutput = "true";
+      this.showOutput();
+    };
+
+    this.shouldClear = function(){
+      var str = JSON.parse(localStorage["testing"]);
+      if(str.length == 0){
+        return false;
+      }
+      if(localStorage.showOutput == "true"){
+        localStorage.showOutput = "false";
+        this.showOutput();
+      }
+      return true;
+    };
+
+    this.addReview = function(dateTime) {
+      if(localStorage.store == "undefined"){
+        this.showOutput();
+        return;
+      }
+      if(dateTime !== null){
+        localStorage.date = dateTime.toLocaleString();
+      }
+
+      var values = ["//" + localStorage.store + ">>", localStorage.total + ">>", localStorage.date];
+
+      var newArray = JSON.parse(localStorage["testing"]);
+      localStorage["testing"] = JSON.stringify(values + newArray );
+      localStorage.showOutput = "true";
+      //this.shouldClear();
+    this.showOutput();
+    //location.reload();
+  };
+
+  this.saveObj = function(){
+      var i;
+      var t = [];
+      localStorage["testing"] = JSON.stringify(t);
+      for(i = this.outputObj.length - 1; i >= 0 ; i--){
+        localStorage.store = this.outputObj[i].store;
+        localStorage.total = this.outputObj[i].total;
+        localStorage.date = this.outputObj[i].date;
+        
+        var values = ["//" + localStorage.store + ">>", localStorage.total + ">>", localStorage.date];
+
+        var newArray = JSON.parse(localStorage["testing"]);
+        localStorage["testing"] = JSON.stringify(values + newArray );
+      }
+    };
+
+    this.removeRow = function(place){
+      this.outputObj.splice(place-1, 1);
+      this.saveObj();
+      this.showOutput();
+    };
+  
+});
+
+var expensesObject = {
+  showDate: false,
+
+  date: new Date(),
+
+  location: ['Babys R Us','Chipotle','Home Depot',
+  'McDonald','Panda Express','Papa Johns',
+  'Pizza Hut','Publix','Shell','Subway',
+  'Walmart','Other'
+  ]
+};
+
+var localObj = []
+var val = {
+  store: {},
+  total: {},
+  date: {}
+
+}
 
 
 app.directive('homePage', function(){
@@ -39,13 +213,13 @@ app.directive('downloadsPage', function(){
 	};
 });
 
-/*app.directive('toolsPage', function(){
+app.directive('expensesPage', function(){
 	return{
 		restrict: 'E',
-		templateUrl: 'public/tools.html'
+		templateUrl: 'public/expenses.html'
 	};
 });
-*/
+
 app.directive('teachingPage', function(){
 	return{
 		restrict: 'E',
@@ -58,4 +232,12 @@ app.directive('contactPage', function(){
 		restrict: 'E',
 		templateUrl: 'public/contact.html'
 	};
+
+
+
+
+
+
 });
+
+
